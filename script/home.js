dc530892a4b1ff8a5a4303920d3970e277acec79
canvast <fengtaotao082913@gmail.com>
2014-08-13 21:32:58 +0800

    导航栏使用真实数据

--- script/home.js
+++ script/home.js
@@ -1,55 +1,70 @@
-App.controller('home', function (page) {
-    var list = MSA.List.getArticles();
-    $p = $(page);
-    $listCont = $(page).find('.js-aritcle-list');
-    $.each(list, function(i, o){
-        if(o.name){
-            $listCont.append('<li class="js-item" data-id="' + o.id + '">' + o.name + '</li>');
-        }
-    })
+window.MSA.home = {
+    init: function(data){
+        this.page = data.page;
+        var list = MSA.List.getArticles();
+        this.categories = MSA.List.getCategories();
+        $p = $(this.page);
+        this.$p = $p;
+        $listCont = $p.find('.js-aritcle-list');
+        $formSearch = $p.find('.js-form-search');
+        $btnSearch = $p.find('.js-btn-search');
+        $btnMenu = $p.find('.js-btn-menu');
 
-    $formSearch = $p.find('.js-form-search');
-    $btnSearch = $p.find('.js-btn-search');
-    $btnSearch.on('click', function(){
-        $searchBtn = $(this);
-        if($searchBtn.hasClass('selected')){
-            $searchBtn.removeClass('selected');
-            $formSearch.hide();
-        }else{
-            $searchBtn.addClass('selected');
-            $formSearch.show();
-        }
-    })
+        $.each(list, function(i, o){
+            if(o.name){
+                $listCont.append('<li class="js-item" data-id="' + o.id + '">' + o.name + '</li>');
+            }
+        })
 
-    $p.on('click', 'li.js-item', function(e){
-        e.isPropagationStopped()
-        $item = $(this);
-        App.load('detail');
-    })
+        $btnSearch.on('click', function(){
+            $searchBtn = $(this);
+            if($searchBtn.hasClass('selected')){
+                $searchBtn.removeClass('selected');
+                $formSearch.hide();
+            }else{
+                $searchBtn.addClass('selected');
+                $formSearch.show();
+            }
+        })
 
-
-    $btnMenu = $p.find('.js-btn-menu');
-    $btnMenu.on('touchend', function(e){
-        $listCont = $p.find('.list-cont');
-        if($listCont.hasClass('move-left')){
-            $listCont.removeClass('move-left');
-        }else{
-            $listCont.addClass('move-left');
-        }
-        e.preventDefault();
-    })
-
-    $p.on('click', 'li.js-menu-item', function(){
-        alert(1);
-    })
-
-    $p.find('.js-aritcle-list').on('touchend', function(){
-        $listCont = $(this);
-        if($listCont.hasClass('move-left')){
-            $listCont.removeClass('move-left');
+        $p.on('click', 'li.js-item', function(e){
+            e.isPropagationStopped()
+            $item = $(this);
+            App.load('detail');
+        })
+        
+        $btnMenu.on('touchend', function(e){
+            $listCont = $p.find('.list-cont');
+            if($listCont.hasClass('move-left')){
+                $listCont.removeClass('move-left');
+            }else{
+                $listCont.addClass('move-left');
+            }
             e.preventDefault();
-        }else{
-            $listCont.addClass('move-left');
-        }
-    })
+        })
+
+        $p.on('click', 'li.js-menu-item', function(){
+            var categoryName = $(this).text();
+            var categoryId = $(this).attr('data-id');
+            
+        })
+
+        this.initCategory();
+    },
+    initCategory: function(){
+        var that = this;
+        this.$categoryListCont = this.$p.find('.js-category-list');
+        this.$categoryListCont.html('');
+        $.each(this.categories, function(i, o){
+            if(o.name){
+                that.$categoryListCont.append('<li class="js-menu-item" data-index="' + i + '" data-id="' + o.id + '">' + o.name + '</li>');
+            }
+        })
+    }
+}
+
+App.controller('home', function (page) {
+    window.MSA.home.init({
+        page: page
+    });
 });
--- script/web/list.js
+++ script/web/list.js
@@ -239,6 +239,113 @@
             ];
 
             return list;
+        },
+        getCategories: function(){
+            var list = [
+                {
+                    "id": 743, 
+                    "open_class": 0, 
+                    "children": [], 
+                    "name": "海事国际公约", 
+                    "description": ""
+                }, 
+                {
+                    "id": 744, 
+                    "open_class": 0, 
+                    "children": [
+                        {
+                            "id": 745, 
+                            "open_class": 0, 
+                            "children": [], 
+                            "name": "法律", 
+                            "description": ""
+                        }, 
+                        {
+                            "id": 746, 
+                            "open_class": 0, 
+                            "children": [], 
+                            "name": "行政法规", 
+                            "description": ""
+                        }, 
+                        {
+                            "id": 747, 
+                            "open_class": 0, 
+                            "children": [], 
+                            "name": "部门规章", 
+                            "description": ""
+                        }, 
+                        {
+                            "id": 748, 
+                            "open_class": 0, 
+                            "children": [], 
+                            "name": "规范性文件", 
+                            "description": ""
+                        }, 
+                        {
+                            "id": 749, 
+                            "open_class": 1, 
+                            "children": [], 
+                            "name": "地方法规、规章、规范性文件", 
+                            "description": ""
+                        }, 
+                        {
+                            "id": 750, 
+                            "open_class": 1, 
+                            "children": [], 
+                            "name": "船舶与海上设施检验规则", 
+                            "description": ""
+                        }, 
+                        {
+                            "id": 751, 
+                            "open_class": 1, 
+                            "children": [], 
+                            "name": "司法解释", 
+                            "description": ""
+                        }
+                    ], 
+                    "name": "法规规范", 
+                    "description": ""
+                }, 
+                {
+                    "id": 752, 
+                    "open_class": 1, 
+                    "children": [], 
+                    "name": "标准文库", 
+                    "description": ""
+                }, 
+                {
+                    "id": 753, 
+                    "open_class": 1, 
+                    "children": [], 
+                    "name": "内部业务工作制度", 
+                    "description": ""
+                },
+                {
+                    "id": 743, 
+                    "open_class": 0, 
+                    "children": [], 
+                    "name": "海事国际公约", 
+                    "description": ""
+                }
+            ];
+            var categories = [];
+            function getCategoryItems(items){
+                var list = [];
+                for(var i = 0, len = items.length; i < len; i++ ){
+                    var item = items[i];
+                    if(item.children != undefined && item.children.length > 0){
+                        list = list.concat(getCategoryItems(item.children));
+                    }else{
+                        list.push({
+                            id: item.id,
+                            name: item.name
+                        });
+                    }
+                }
+                return list;
+            }
+            categories = getCategoryItems(list);
+            return categories;
         }
     }
 })()
